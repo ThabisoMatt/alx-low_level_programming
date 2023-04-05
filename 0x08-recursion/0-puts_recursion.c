d41ed4417d64f28f694d66e41ecd0f93a4e1785b
ThabisoMatt <thabisomatthews9@gmail.com>
2023-04-05 01:48:29 -0700

    Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,18 +2,21 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0') { // base case: end of string
+	if (*s == '\0')
+	{
 		return;
 	}
-	else 
+	else
 	{
-		_print_rev_recursion(s+1); // recursive call to print rest of string
-		putchar(*s); // print current character
+		_print_rev_recursion('s + 1');
+		putchar(*s);
 	}
 }
-int main()
+int main(void)
 {
 	char str[] = "Thabiso, Matthews!";
-	_print_rev_recursion(str);
+
+		_print_rev_recursion(str);
+
 	return 0;
 }
